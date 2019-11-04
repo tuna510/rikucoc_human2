1fe8a6506f80f67f5d984548c47afee9ab5f8f62
tuna510 <56784079+tuna510@users.noreply.github.com>
2019-11-04 16:01:59 +0900

    Add files via upload

---
+++ js/file1.js
@@ -0,0 +1,22 @@
+//ヘッダーの高さ分だけコンテンツを下げる
+$(function () {
+    var height = $("#header").height();
+    // 100px余裕を持たせる
+    $("body").css("margin-top", height + 100);
+});
+
+// ---------------------------------------
+
+// スクロール
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - 100;
+        var speed = 600;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+});
