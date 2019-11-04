eb39427019b85e10f4f4fcb36b3e5579520adfd4
tuna510 <56784079+tuna510@users.noreply.github.com>
2019-11-05 04:14:25 +0900

    Update file1.js

--- js/file1.js
+++ js/file1.js
@@ -24,17 +24,30 @@ $(function () {
 // ------------------------------------------
 // 文字がふわっと出てくる
 $(function () {
-
     $(window).scroll(function () {
-
         $(".fade_off").each(function () {
             var imgPos = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > imgPos - windowHeight + windowHeight - 350) {
+            if (scroll > imgPos - windowHeight + windowHeight / 2.5) {
                 $(this).addClass("fade_on");
             } else {
                 $(this).removeClass("fade_on");
+            }
+        });
+    });
+});
+// ------------------------------------------
+$(function () {
+    $(window).scroll(function () {
+        $(".fadee_off").each(function () {
+            var imgPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > imgPos - windowHeight + windowHeight / 5) {
+                $(this).addClass("fadee_on");
+            } else {
+                $(this).removeClass("fadee_on");
             }
         });
     });
